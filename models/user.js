72c0bd5b1e665e37d11ccac022328700f821ba30
Jesse Zelaya <jessezelaya1@gmail.com>
2015-08-11 11:09:46 -0700

    Created user schema and model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+//USER MODEL
+//IMPORT mongoose and bcrypt
+var mongoose = require('mongoose');
+var bcrypt =   require('bcrypt');
+
+//Create user schema
+var userSchema = new mongoose.Schema({
+  name: String,
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true, select: false},
+  passHint: [{question: String, answer: String}]
+});
+
+//Create user model out of userSchema (constructor function)
+var User = mongoose.model('User',userSchema);
+
+//Export the model for use within app
+module.exports = User:
