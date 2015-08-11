47d9388d5f03fcb20d55aff505eca2f345225d17
Jesse Zelaya <jessezelaya1@gmail.com>
2015-08-11 11:44:14 -0700

    Created user encryption

--- models/user.js
+++ models/user.js
@@ -11,6 +11,26 @@ var userSchema = new mongoose.Schema({
   passHint: [{question: String, answer: String}]
 });
 
+//Ecrypting user password method
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  //Hash the password only if the password has been changed or is new
+  if(!user.isModified('password')) return next();
+
+  //Generate salt
+  bcrypt.genSalt(5, function (err,salt) {
+    if(err) return next(err);
+    bcrypt.hash(user.password, salt, function (err,hash) {
+      if(err) return next(err);
+
+      //change the password to the hased version
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 //Create user model out of userSchema (constructor function)
 var User = mongoose.model('User',userSchema);
 
